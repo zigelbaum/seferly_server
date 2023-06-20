76c03234531dc7f4d93d8bf5d8ecc172795d7cbc
Elisheva (Aronstam) Sherman <57481785+ElishAronstam@users.noreply.github.com>
2023-06-20 11:08:46 +0300

    added categories

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -5,7 +5,7 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(`mongodb+srv://elisheva777:eli777me@cluster1.mqgr2av.mongodb.net/seferly`);
-  console.log("mongo connect ")
+  console.log("mongo connect black 24 a")
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
 
