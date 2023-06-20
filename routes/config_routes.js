05b3e3dd4c6e8a960b5a2b68355eef017c803bf3
zigelbaum <chanush850@gmail.com>
2023-06-20 11:54:22 +0300

    config routes fixed cities

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,13 +1,14 @@
 const indexR = require("./index");
 const usersR = require("./users");
 const booksR = require("./books");
-const subjectsR=require("./subjects")
+const subjectsR=require("./subjects");
+const citiesR = require("./cities")
 
 exports.routesInit = (app) => {
   app.use("/",indexR);
   app.use("/users",usersR);
   app.use("/books",booksR);
   app.use("/subjects",subjectsR);
-  app.use("/cities",cityR);
+  app.use("/cities",citiesR);
   
 }
