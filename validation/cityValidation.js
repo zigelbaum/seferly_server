2ac3b22978c5aafbf410a2e79dcfeece5095c519
Elisheva (Aronstam) Sherman <57481785+ElishAronstam@users.noreply.github.com>
2023-06-20 12:58:27 +0300

    deleted cities

--- validation/cityValidation.js
+++
@@ -1,7 +0,0 @@
-const Joi = require("joi");
-exports.validateCity = (_reqBody) => {
-    let joiSchema = Joi.object({
-      name:Joi.string().min(2).max(99).required()
-    })
-    return joiSchema.validate(_reqBody);
-  }
