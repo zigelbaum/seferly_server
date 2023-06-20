cadbcbdbfcb755770fce06a2c7b87afe8b530fc5
Atara Rose <ataraeg613@gmail.com>
2023-06-20 13:23:28 +0300

    uploads model,router,validation. test on postman

---
+++ validation/uploadValidation.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+
+exports.validateUpload = (_reqBody) => {
+
+    let joiSchema = Joi.object({   
+        price: Joi.number().min(2).max(99).allow(null, ""),
+        img_url: Joi.string().min(2).max(200).allow(null, ""),
+        info: Joi.string().min(1).max(400).allow(null, ""),
+    })
+    return joiSchema.validate(_reqBody);
+}
