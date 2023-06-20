44ba9ef51854396528b18611d510b4c28b3c4c93
Atara Rose <ataraeg613@gmail.com>
2023-06-20 11:17:46 +0300

    mongodb

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -4,7 +4,7 @@ const {config} = require("../config/secret")
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://chaniV:chaniVmongo@cluster0.ijk1hfe.mongodb.net/seferly`);
+  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.ijk1hfe.mongodb.net/seferly`);
   console.log("mongo connect black 24 a")
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
