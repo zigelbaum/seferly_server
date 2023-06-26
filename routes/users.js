aa4c7c023ea9ccbc1c19763c9333f650cedacfda
zigelbaum <chanush850@gmail.com>
2023-06-26 20:01:03 +0300

    added console log to checkToken route

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.get("/myInfo", auth, async (req, res) => {
 
 //returns token data
 router.get("/checkToken", auth, async (req, res) => {
-    // console.log(req.tokenData)
+    console.log(req.tokenData)
     res.status(200).json(req.tokenData);
   })
 
